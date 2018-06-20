a94ed757372c4461994e6b8f3d95e7ebd098d680
Rajput763 <rajputvivek763@gmail.com>
2018-06-20 20:11:31 +0530

    class assignment

--- class 5/Substring.java
+++ class 5/Substring.java
@@ -8,7 +8,7 @@ Scanner x= new Scanner(System.in);
 String y=x.nextLine();
 for(int i=0;i<y.length();i++)
 {
-for(int j=1;j<y.length()-i;j++){
+for(int j=1;j<=y.length()-i;j++){
  String s=y.substring(i,i+j);
 System.out.println(s);
 }
